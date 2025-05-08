79d5c46b3efc724049e027fba333f3873dbc4c51
Kostalova <mail.katerina.kostalova@gmail.com>
2025-05-08 15:47:29 +0200

    Class-Based View.

--- viewer/views.py
+++ viewer/views.py
@@ -1,21 +1,18 @@
+from msilib.schema import ListView
 from django.shortcuts import render
-
 from viewer.models import Habit
 
 
 
-
+#FUNKCIONALITA
 def home(request):
     return render(request, 'home.html')
-#FUNKCIONALITA
-def habits(request):
-    habits_ = Habit.objects.all() # vytahnu si z databaze všechny filmy
-    context = {'habits': habits_} #potřebuju do te templaty poslat data, nazev proměnné 'habits' : tohle je obsa=) habits_
-    return render(request=request,#potřebuju poslat request přišel mi od uživatele požadavek, já ho zpracuju a pošlu ho zpátky
-                  template_name='habits.html', # tamplate ktera se mi použije pro to zobrazení
-                  context=context)#potřebuju data pro to zobrazení
 
-#1.řádek fce vytahne z databaze všechny filmy=)
-#2. =) vloží je do nějakého slovníku balíku
-#3. =) tím render se pošle do té templaty
+
+class HabitsListView(ListView):# dělám to objektově -  chci seznam = listview
+    template_name = 'habits.html'# do jaké temlaty posílám
+    model = Habit#z jaké tabulky to vytahuju
+    #pozor do tamplate se posilaji data pod nazvem 'object_list', mužu nechat pouze "template_name a model a vpravit přímotepllate {% for habit in 'object_list'%}
+    # nebo přidat řádek context_object_name a definovat to zde
+    context_object_name = 'habits'
 
